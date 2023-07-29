5b21fbf30377cb4169f51439927d3309231cb3fe
22050305-Yangziyi <2420769218@qq.com>
2023-07-29 20:44:13 +0800

    NJU-ProjectN/nanos-lite ics2021 initialized
    NJU-ProjectN/nanos-lite 2a141760e31be246a7316942293a97873925bc2f Makefile: use header files in newlib-navy

--- nanos-lite/src/ramdisk.c
+++ nanos-lite/src/ramdisk.c
@@ -3,15 +3,13 @@
 extern uint8_t ramdisk_start;
 extern uint8_t ramdisk_end;
 #define RAMDISK_SIZE ((&ramdisk_end) - (&ramdisk_start))
+
 /* The kernel is monolithic, therefore we do not need to
  * translate the address `buf' from the user process to
  * a physical one, which is necessary for a microkernel.
  */
 
 /* read `len' bytes starting from `offset' of ramdisk into `buf' */
-//该函数实现了从内存磁盘中读取数据的功能。函数参数包括一个指向缓冲区的指针buf，
-// 偏移量offset和要读取的字节数len。该函数使用assert宏来确保偏移量和字节数不会导致读取超出内存磁盘的范围。
-// 函数使用memcpy函数从内存磁盘中的指定位置复制数据到缓冲区，并返回读取的字节数。
 size_t ramdisk_read(void *buf, size_t offset, size_t len) {
   assert(offset + len <= RAMDISK_SIZE);
   memcpy(buf, &ramdisk_start + offset, len);
@@ -19,9 +17,6 @@ size_t ramdisk_read(void *buf, size_t offset, size_t len) {
 }
 
 /* write `len' bytes starting from `buf' into the `offset' of ramdisk */
-// 该函数实现了向内存磁盘中写入数据的功能。函数参数包括一个指向缓冲区的指针buf，
-// 偏移量offset和要写入的字节数len。该函数使用assert宏来确保偏移量和字节数不会导致写入超出内存磁盘的范围。
-// 函数使用memcpy函数将数据从缓冲区复制到内存磁盘中的指定位置，并返回写入的字节数。
 size_t ramdisk_write(const void *buf, size_t offset, size_t len) {
   assert(offset + len <= RAMDISK_SIZE);
   memcpy(&ramdisk_start + offset, buf, len);
--- navy-apps/libs/libminiSDL/src/event.c
+++ navy-apps/libs/libminiSDL/src/event.c
@@ -1,5 +1,6 @@
 #include <NDL.h>
 #include <SDL.h>
+#include <assert.h>
 
 #define keyname(k) #k,
 
@@ -8,16 +9,62 @@ static const char *keyname[] = {
   _KEYS(keyname)
 };
 
+static uint8_t keystate[sizeof(keyname) / sizeof(char *)];
+
 int SDL_PushEvent(SDL_Event *ev) {
   return 0;
 }
 
 int SDL_PollEvent(SDL_Event *ev) {
+  char buf[64];
+  if(NDL_PollEvent(buf, sizeof(buf))){
+    char keydown[3];
+    char event_keyname[16];
+    printf("%s\n", buf);
+    sscanf(buf, "%s %s\n", keydown, event_keyname);
+    if(!strcmp(keydown, "kd")){
+      ev->type = SDL_KEYDOWN;
+    }else if(!strcmp(keydown, "ku")){
+      ev->type = SDL_KEYUP;
+    }else{
+      printf("keydown: %s\n", keydown);
+      assert(0);
+    }
+    for (int i=0; i<sizeof(keyname)/sizeof(char*); i++){
+      if (!strcmp(event_keyname, keyname[i])){
+        ev->key.keysym.sym = i;
+        keystate[i] = ev->type == SDL_KEYDOWN ? 1 : 0;
+        return 1;
+      }
+    }
+    assert(0);
+  }
   return 0;
 }
 
 int SDL_WaitEvent(SDL_Event *event) {
-  return 1;
+  char buf[64];
+  while (!NDL_PollEvent(buf, sizeof(buf)));
+
+  char keydown[3];
+  char event_keyname[16];
+  sscanf(buf, "%s %s\n", keydown, event_keyname);
+  if(!strcmp(keydown, "kd")){
+    event->type = SDL_KEYDOWN;
+  }else if(!strcmp(keydown, "ku")){
+    event->type = SDL_KEYUP;
+  }else{
+    printf("keydown: %s\n", keydown);
+    assert(0);
+  }
+  for (int i=0; i<sizeof(keyname)/sizeof(char*); i++){
+    if (!strcmp(event_keyname, keyname[i])){
+      event->key.keysym.sym = i;
+      keystate[i] = (event->type == SDL_KEYDOWN) ? 1 : 0;
+      return 1;
+    }
+  }
+  return 0;
 }
 
 int SDL_PeepEvents(SDL_Event *ev, int numevents, int action, uint32_t mask) {
@@ -25,5 +72,8 @@ int SDL_PeepEvents(SDL_Event *ev, int numevents, int action, uint32_t mask) {
 }
 
 uint8_t* SDL_GetKeyState(int *numkeys) {
-  return NULL;
+  if (numkeys != NULL){
+    *numkeys = sizeof(keystate) / sizeof(uint8_t);
+  }
+  return keystate;
 }
