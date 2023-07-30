728d4537c27ec5878d4ecd903ec61272424d52ba
22050305-Yangziyi <2420769218@qq.com>
2023-07-30 20:09:17 +0800

    0730.1

--- nanos-lite/src/syscall.c
+++ nanos-lite/src/syscall.c
@@ -1,10 +1,29 @@
 #include <common.h>
 #include "syscall.h"
+
+
 void do_syscall(Context *c) {
   uintptr_t a[4];
   a[0] = c->GPR1;
-
+  a[1] = c->GPR2;
+  a[2] = c->GPR3;
+  a[3] = c->GPR4;
+#ifdef STRACE
+  printf("syscall ID = %d\n", a[0]);
+#endif
   switch (a[0]) {
+    case SYS_exit: yield(); c->GPRx = 0; break;
+    case SYS_yield: yield(); c->GPRx = 0; break;
+    // case SYS_open: c->GPRx = fs_open((char*)a[1], a[2], a[3]); break;
+    // case SYS_read: c->GPRx = fs_read(a[1], (void*)a[2], a[3]); break;
+    // case SYS_write: c->GPRx = fs_write(a[1], (void*)a[2], a[3]); break;
+    // case SYS_close: c->GPRx = fs_close(a[1]); break;
+    // case SYS_lseek: c->GPRx = fs_lseek(a[1], a[2], a[3]); break;
+    // case SYS_brk: c->GPRx = 0; break;
+    // case SYS_execve: naive_uload(NULL, (char*)a[1]); break;
+    // case SYS_gettimeofday: 
+    //   c->GPRx = gettime((struct timeval *)a[1], (struct timezone *)a[2]); 
+    //   break;
     default: panic("Unhandled syscall ID = %d", a[0]);
   }
 }
