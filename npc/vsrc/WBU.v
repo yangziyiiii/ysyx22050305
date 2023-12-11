4578c905ff7cf940d84e0cc439137eb1991b54b4
master <9840334+stevennn2@user.noreply.gitee.com>
2023-12-11 19:53:35 +0800

    pipeline_wrong_rebuild

---
+++ npc/vsrc/WBU.v
@@ -0,0 +1,10 @@
+module WBU(
+
+);
+    
+
+
+
+    
+endmodule //WBU
+
--- ysyx-exam/setup-env.sh
+++
@@ -1,4 +0,0 @@
-export NEMU_HOME=`readlink -e nemu`
-export AM_HOME=`readlink -e abstract-machine`
-export NAVY_HOME=`readlink -e navy-apps`
-export NPC_HOME=`readlink -e npc`
